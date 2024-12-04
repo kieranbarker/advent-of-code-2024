60f4ba023582e7d5f4983ac4ab7bd3e461dccd42
Kieran Barker <kieran11397@gmail.com>
2024-12-04 18:26:22 -0500

    Update Day 4 Part 1

--- day-04/part-01.ts
+++ day-04/part-01.ts
@@ -7,56 +7,48 @@ for (let i = 0; i < grid.length; i++) {
   for (let j = 0; j < grid[i].length; j++) {
     if (grid[i][j] !== "X") continue;
 
-    // Vertical upwards
     if (
       grid[i - 1]?.[j] === "M" &&
       grid[i - 2]?.[j] === "A" &&
       grid[i - 3]?.[j] === "S"
     ) matches++;
 
-    // Diagonal upwards right
     if (
       grid[i - 1]?.[j + 1] === "M" &&
       grid[i - 2]?.[j + 2] === "A" &&
       grid[i - 3]?.[j + 3] === "S"
     ) matches++;
 
-    // Horizontal right
     if (
       grid[i][j + 1] === "M" &&
       grid[i][j + 2] === "A" &&
       grid[i][j + 3] === "S"
     ) matches++;
 
-    // Diagonal downwards right
     if (
       grid[i + 1]?.[j + 1] === "M" &&
       grid[i + 2]?.[j + 2] === "A" &&
       grid[i + 3]?.[j + 3] === "S"
     ) matches++;
 
-    // Vertical downwards
     if (
       grid[i + 1]?.[j] === "M" &&
       grid[i + 2]?.[j] === "A" &&
       grid[i + 3]?.[j] === "S"
     ) matches++;
 
-    // Diagonal downards left
     if (
       grid[i + 1]?.[j - 1] === "M" &&
       grid[i + 2]?.[j - 2] === "A" &&
       grid[i + 3]?.[j - 3] === "S"
     ) matches++;
 
-    // Horizontal left
     if (
       grid[i][j - 1] === "M" &&
       grid[i][j - 2] === "A" &&
       grid[i][j - 3] === "S"
     ) matches++;
 
-    // Diagonal upwards left
     if (
       grid[i - 1]?.[j - 1] === "M" &&
       grid[i - 2]?.[j - 2] === "A" &&
