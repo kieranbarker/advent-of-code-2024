c0781c509a12d23c4b4ea6fe4c43e4dd202a2e50
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2024-12-10 09:17:14 +0000

    Complete Day 7 Part 2

---
+++ day-07/part-02.ts
@@ -0,0 +1,42 @@
+const input = await Deno.readTextFile("input.txt");
+const equations = new Map<number, number[]>();
+
+for (const equation of input.split("\n")) {
+  const [x, nums] = equation.split(": ");
+  equations.set(Number(x), nums.split(" ").map(Number));
+}
+
+function calculateResults(nums: number[], current: number, index: number) {
+  if (index === nums.length) return new Set([current]);
+
+  const next = nums[index];
+  const results = new Set<number>();
+
+  for (const result of calculateResults(nums, current + next, index + 1)) {
+    results.add(result);
+  }
+
+  for (const result of calculateResults(nums, current * next, index + 1)) {
+    results.add(result);
+  }
+
+  const concatenated = Number(String(current) + String(next));
+
+  for (const result of calculateResults(nums, concatenated, index + 1)) {
+    results.add(result);
+  }
+
+  return results;
+}
+
+let total = 0;
+
+for (const [target, nums] of equations) {
+  const results = calculateResults(nums, nums[0], 1);
+
+  if (results.has(target)) {
+    total += target;
+  }
+}
+
+console.log(total);
