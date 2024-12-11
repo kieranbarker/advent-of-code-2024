68c68d08467355f622a35b39d9bc207a9b032a77
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2024-12-11 13:47:42 +0000

    Complete Day 9 Part 1

--- day-09/part-01.ts
+++ day-09/part-01.ts
@@ -1,44 +1,44 @@
 const input = await Deno.readTextFile("input.txt");
 
-let blocks = generateBlocks(input);
-blocks = defragment(blocks);
+const blocks = generateBlocks(input);
+fragment(blocks);
 
 const checksum = generateChecksum(blocks);
 console.log(checksum);
 
 function generateBlocks(input: string) {
-  let blocks = "";
+  const blocks: (number | string)[] = [];
   let fileId = 0;
 
   for (let i = 0; i < input.length; i++) {
     if (i % 2 === 0) {
-      blocks += String(fileId).repeat(Number(input[i]));
+      blocks.push(...new Array<number>(Number(input[i])).fill(fileId));
       fileId++;
     } else {
-      blocks += ".".repeat(Number(input[i]));
+      blocks.push(...new Array<string>(Number(input[i])).fill("."));
     }
   }
 
   return blocks;
 }
 
-function generateDots(blocks: string) {
+function countDots(blocks: (number | string)[]) {
   let count = 0;
 
-  for (const char of blocks) {
-    if (char === ".") {
+  for (const block of blocks) {
+    if (block === ".") {
       count++;
     }
   }
 
-  return ".".repeat(count);
+  return count;
 }
 
-function findLastIndex(str: string, regex: RegExp) {
-  let lastIndex: number | undefined;
+function findLastIndex(blocks: (number | string)[]) {
+  let lastIndex = -1;
 
-  for (let i = str.length - 1; i > -1; i--) {
-    if (regex.test(str[i])) {
+  for (let i = blocks.length - 1; i > -1; i--) {
+    if (Number.isInteger(blocks[i])) {
       lastIndex = i;
       break;
     }
@@ -47,25 +47,22 @@ function findLastIndex(str: string, regex: RegExp) {
   return lastIndex;
 }
 
-function defragment(blocks: string) {
-  const dots = generateDots(blocks);
-  const regex = /\d/;
+function fragment(blocks: (number | string)[]) {
+  const dotCount = countDots(blocks);
 
   let index = blocks.indexOf(".");
-  let lastIndex = findLastIndex(blocks, regex)!;
+  let lastIndex = findLastIndex(blocks);
 
-  while (!blocks.endsWith(dots)) {
-    blocks = blocks.slice(0, index) + blocks[lastIndex] +
-      blocks.slice(index + 1, lastIndex) + "." + blocks.slice(lastIndex + 1);
-
+  while (blocks.slice(-dotCount).some((block => block !== "."))) {
+    [blocks[index], blocks[lastIndex]] = [blocks[lastIndex], blocks[index]];
     index = blocks.indexOf(".");
-    lastIndex = findLastIndex(blocks, regex)!;
+    lastIndex = findLastIndex(blocks);
   }
 
   return blocks;
 }
 
-function generateChecksum(blocks: string) {
+function generateChecksum(blocks: (number | string)[]) {
   let checksum = 0;
 
   for (let i = 0; i < blocks.length; i++) {
@@ -73,7 +70,7 @@ function generateChecksum(blocks: string) {
       break;
     }
 
-    checksum += i * Number(blocks[i]);
+    checksum += i * (blocks[i] as number);
   }
 
   return checksum;
